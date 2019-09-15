dba8a7e59024b2f6ff4ab17d8f3a1d212d3f3411
Eundo Lee <cobed95@gmail.com>
2019-09-15 16:21:19 +0900

    stack queue added

---
+++ data-structure/Queue/Queue.java
@@ -0,0 +1,29 @@
+public class Queue {
+    private int[] array;
+    private int head;
+    private int tail;
+
+    public Queue(int length) {
+        this.array = new int[length];
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO: implement this.
+        return true;
+    }
+
+    public boolean isFull() {
+        // TODO: implement this.
+        return true;
+    }
+    public void enqueue(int data) {
+        // TODO: implement this.
+    }
+
+    public int dequeue() {
+        // TODO: implement this.
+        return -1;
+    }
+}
---
+++ data-structure/Stack/Stack.java
@@ -0,0 +1,33 @@
+public class Stack {
+    private int[] array;
+    private int head;
+
+    public Stack(int length) {
+        this.array = new int[length];
+        this.head = 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO: implement this.
+        return true;
+    }
+
+    public boolean isFull() {
+        // TODO: implement this.
+        return true;
+    }
+
+    public void push(int data) {
+        // TODO: implement this.
+    }
+
+    public int pop() {
+        // TODO: implement this.
+        return -1;
+    }
+
+    public int peek() {
+        // TODO: implement this.
+        return -1;
+    }
+}
