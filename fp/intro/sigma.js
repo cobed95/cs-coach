8362eca11c94c874420a2b8f74a255249af8e3ff
Eundo Lee <cobed95@gmail.com>
2019-10-30 00:37:41 +0900

    fp first examples added

---
+++ fp/intro/sigma.js
@@ -0,0 +1,12 @@
+/**
+ * 시그마 함수를 구현하시오.
+ * 조건: for 반복문 혹은 while 반복문을 써서는 안되며
+ * 모든 변수는 const 로 선언돼야함. (변수가 존재하지 않음)
+ * @param a: sigma의 시작 정수
+ * @param b: sigma의 끝 정수
+ * @param f: 정수를 받아 정수를 되돌려줄 함수
+ */
+
+const simga = (a, b, f) => {
+    // TODO: implement Sigma!!
+}
