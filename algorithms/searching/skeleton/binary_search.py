04e293ce4b2ef150c9a858cb9fbafb96a632a523
Eundo Lee <cobed95@gmail.com>
2019-11-03 17:36:41 +0900

    added testcases

--- algorithms/searching/skeleton/binary_search.py
+++ algorithms/searching/skeleton/binary_search.py
@@ -22,8 +22,18 @@ def binary_search(int_list, target, start, end):
 
 
 def main():
+    # Test01: target is in the list.
     int_list = [1, 2, 6, 12, 67, 199, 233, 651, 1422, 32010]
     print(binary_search(int_list, 233, 0, len(int_list)))
+
+    # Test02: target is not in the list.
+    print(binary_search(int_list, 3, 0, len(int_list)))
+
+    # Test03: target is larger than the maximum value in list.
+    print(binary_search(int_list, 40000, 0, len(int_list)))
+
+    # Test04: target is smaller than the minimum value in list.
+    print(binary_search(int_list, -1, 0, len(int_list)))
 
 if __name__ == "__main__":
     main()
