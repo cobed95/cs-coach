fda1bb85a53cbee3a64fdab60a7853a9f8aab565
Eundo Lee <cobed95@gmail.com>
2019-11-03 16:32:43 +0900

    skeleton for binary_search added

---
+++ algorithms/searching/skeleton/binary_search.py
@@ -0,0 +1,9 @@
+def binary_search(int_list, target, start, end):
+    # TODO: Fill this in.
+
+def main:
+    int_list = [1, 2, 6, 12, 67, 199, 233, 651, 1422, 32010]
+    print(binary_search(int_list, 233, 0, len(int_list)))
+
+if __name__ == "__main__":
+    main()
